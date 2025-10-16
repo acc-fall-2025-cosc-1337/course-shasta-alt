3965c783e0fd160defd7adc694b1847becf9a3db
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-16 11:33:26 -0500

    Implement DNA analysis menu in main function
    Added a DNA analysis menu with options for GC content and DNA complement.

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,46 @@
+#include <iostream>
+#include "func.h"
+using namespace std;
 
-int main() 
+int main()
 {
+    int choice;
+    string dna;
 
-	return 0;
+    do
+    {
+        cout << "\nDNA Analysis Menu\n";
+        cout << "1 - Get GC Content\n";
+        cout << "2 - Get DNA Complement\n";
+        cout << "3 - Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            double gc = get_gc_content(dna);
+            cout << "GC Content: " << gc << endl;
+        }
+        else if (choice == 2)
+        {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            string complement = get_dna_complement(dna);
+            cout << "DNA Complement: " << complement << endl;
+        }
+        else if (choice == 3)
+        {
+            cout << "Exiting program...\n";
+        }
+        else
+        {
+            cout << "Invalid option. Try again.\n";
+        }
+
+    } while (choice != 3);
+
+    return 0;
 }
+
