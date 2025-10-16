2d7ce406f8dd5ab97ccd3514f0f55b2e06ed15eb
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-15 19:50:11 -0500

    Implement multiplication and output in main.cpp
    Added functionality to multiply numbers and display results.

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,19 @@
-﻿//write include statements
+// main.cpp
+#include <iostream>
+#include "data_types.h"
 
-
-//write namespace using statement for cout
-
+using namespace std;
 
 int main()
 {
-	
+    int result;
 
-	return 0;
+    result = multiply_numbers(7, 7);
+    cout << "7 x 7 = " << result << endl;
+
+    result = multiply_numbers(5, 5);
+    cout << "5 x 5 = " << result << endl;
+
+    return 0;
 }
+
