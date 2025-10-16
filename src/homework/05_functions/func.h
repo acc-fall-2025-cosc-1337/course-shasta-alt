057ea16ae3e6ba32466a8304990a6f6110d04b0d
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-16 11:32:19 -0500

    Add function prototypes for DNA processing

--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,13 @@
-//add function(s) prototype here
+// func.h
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <string>
+using std::string;
+
+// Function prototypes
+double get_gc_content(const string& dna);
+string get_reverse_string(string dna);
+string get_dna_complement(string dna);
+
+#endif
