2a2165b00e609f6c871d1475e956ed964f95475e
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-15 19:50:58 -0500

    Replace sum_numbers tests with multiply_numbers tests

--- test/homework/01_data_types/01_data_types_tests.cpp
+++ test/homework/01_data_types/01_data_types_tests.cpp
@@ -1,13 +1,13 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+// 01_data_types_tests.cpp
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "data_types.h"
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+
+TEST_CASE("Verify multiply_numbers function")
+{
+    REQUIRE(multiply_numbers(7, 7) == 49);
+    REQUIRE(multiply_numbers(5, 5) == 25);
+    REQUIRE(multiply_numbers(10, 2) == 20);
 }
 
-TEST_CASE("Verify sum_numbers function") 
-{
-	REQUIRE(add_numbers(5, 5) == 10);
-	REQUIRE(add_numbers(10, 10) == 20);
-}
 
