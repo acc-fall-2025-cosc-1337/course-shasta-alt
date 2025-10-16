80a1167dffbcf9495d57b6c750b990bd69e97b02
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-16 11:32:36 -0500

    Implement DNA analysis functions in func.cpp

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,49 @@
-//add include statements
+#include "func.h"
 
-//add function code here
+double get_gc_content(const string& dna)
+{
+    int gc_count = 0;
+    for (char nucleotide : dna)
+    {
+        if (nucleotide == 'C' || nucleotide == 'G')
+        {
+            gc_count++;
+        }
+    }
+
+    return static_cast<double>(gc_count) / dna.size();
+}
+
+string get_reverse_string(string dna)
+{
+    string reversed = "";
+    for (int i = dna.size() - 1; i >= 0; i--)
+    {
+        reversed += dna[i];
+    }
+    return reversed;
+}
+
+string get_dna_complement(string dna)
+{
+    string reversed_dna = get_reverse_string(dna);
+    for (char &nucleotide : reversed_dna)
+    {
+        switch (nucleotide)
+        {
+        case 'A':
+            nucleotide = 'T';
+            break;
+        case 'T':
+            nucleotide = 'A';
+            break;
+        case 'C':
+            nucleotide = 'G';
+            break;
+        case 'G':
+            nucleotide = 'C';
+            break;
+        }
+    }
+    return reversed_dna;
+}
