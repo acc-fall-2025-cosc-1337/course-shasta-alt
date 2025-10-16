c8360d5d1a55558d6479372b5739e95553b53ae5
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-15 19:49:36 -0500

    Replace add function with multiply function

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -1,12 +1,11 @@
+// data_types.cpp
 #include "data_types.h"
 
-//example
-int add_numbers(int num1, int num2)
+int multiply_numbers(int num1, int num2)
 {
-	return num1 + num2;
+    return num1 * num2;
 }
 
-//write function code here
 
 
 
