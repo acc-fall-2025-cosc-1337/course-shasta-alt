221621c5e0d963ffb7c11eb760cda6d7da3d9856
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-11-05 19:00:02 -0600

    Update arrays.h

--- src/homework/07_arrays/arrays.h
+++ src/homework/07_arrays/arrays.h
@@ -1,1 +1,34 @@
-//add function(s) prototype
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+#include <string>
+#include <vector>
+
+class TicTacToe
+{
+public:
+    // public interface
+    void start_game(std::string first_player);
+    void mark_board(int position);
+    bool game_over();
+    std::string get_player() const;
+    void display_board() const;
+    std::string get_winner() const;
+
+private:
+    // helpers
+    bool check_column_win();
+    bool check_row_win();
+    bool check_diagonal_win();
+    bool check_board_full();
+    void set_next_player();
+    void clear_board();
+    void set_winner();
+
+    // data
+    std::string player;                 // "X" or "O"
+    std::vector<std::string> pegs{9, " "}; // 9 spaces initially
+    std::string winner;                 // "X", "O", or "C" for tie
+};
+
+#endif
