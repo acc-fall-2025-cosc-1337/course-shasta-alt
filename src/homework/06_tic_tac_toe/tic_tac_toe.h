9d0dc5de740063743bbfb80c087ecb6df12ca695
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-11-12 19:19:53 -0600

    Update tic_tac_toe.h

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,34 @@
-//h
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(std::string first_player);
+    void mark_board(int position);
+    std::string get_player() const;
+    void display_board() const;
+    std::string get_winner() const;
+
+private:
+    std::string player;
+    std::string winner;
+    std::vector<std::string> pegs{9, " "};
+
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+
+    bool check_row_win();
+    bool check_column_win();
+    bool check_diagonal_win();
+
+    void set_winner();
+};
+
+#endif
