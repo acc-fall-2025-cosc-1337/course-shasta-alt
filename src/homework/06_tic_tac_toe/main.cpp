d81b5b0926512efc85c6deb5ce1c95fd3412e7aa
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-11-12 19:20:38 -0600

    Update main.cpp

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,38 @@
+#include "tic_tac_toe.h"
+#include <iostream>
 
-int main() 
+int main()
 {
+    TicTacToe game;
+    std::string first;
+    char again = 'Y';
 
-	return 0;
+    while (again == 'Y' || again == 'y')
+    {
+        do
+        {
+            std::cout << "Enter first player (X or O): ";
+            std::cin >> first;
+        }
+        while (first != "X" && first != "O");
+
+        game.start_game(first);
+
+        int position = 0;
+
+        while (!game.game_over())
+        {
+            std::cout << "Enter position (1-9): ";
+            std::cin >> position;
+            game.mark_board(position);
+            game.display_board();
+        }
+
+        std::cout << "Winner: " << game.get_winner() << "\n";
+
+        std::cout << "Play again? (Y/N): ";
+        std::cin >> again;
+    }
+
+    return 0;
 }
