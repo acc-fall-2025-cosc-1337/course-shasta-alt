83581bac12c85f3343e6787128dddb693033851f
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-11-18 19:58:58 -0600

    Update tic_tac_toe_manager.h

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,23 @@
-//h
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+#include "tic_tac_toe.h"
+#include <vector>
+#include <string>
+
+class TicTacToeManager
+{
+public:
+    void save_game(TicTacToe game);
+    void get_winner_total(int& x, int& o, int& t);
+
+private:
+    std::vector<TicTacToe> games;
+    int x_win{0};
+    int o_win{0};
+    int ties{0};
+
+    void update_winner_count(std::string winner);
+};
+
+#endif
