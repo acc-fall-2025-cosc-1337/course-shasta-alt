58b6a38c38e3dc530186329611c52647dfa855d1
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-11-18 19:59:12 -0600

    Update tic_tac_toe_manager.cpp

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,31 @@
-//cpp
+#include "tic_tac_toe_manager.h"
+
+void TicTacToeManager::save_game(TicTacToe game)
+{
+    std::string winner = game.get_winner();
+    update_winner_count(winner);
+    games.push_back(game);
+}
+
+void TicTacToeManager::update_winner_count(std::string winner)
+{
+    if (winner == "X")
+    {
+        x_win++;
+    }
+    else if (winner == "O")
+    {
+        o_win++;
+    }
+    else
+    {
+        ties++;
+    }
+}
+
+void TicTacToeManager::get_winner_total(int& x, int& o, int& t)
+{
+    x = x_win;
+    o = o_win;
+    t = ties;
+}
