a2d28d2bd4a2b92dbc0a437d4907e68d0cbf05ed
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-12-13 21:09:20 -0600

    Refactor TicTacToe class and update access specifiers

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,34 +1,34 @@
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
 
-#include <string>
 #include <vector>
+#include <string>
 #include <iostream>
 
 class TicTacToe
 {
 public:
-    bool game_over();
+    TicTacToe(int size);
     void start_game(std::string first_player);
     void mark_board(int position);
+    bool game_over();
     std::string get_player() const;
-    void display_board() const;
     std::string get_winner() const;
+    void display_board() const;
 
-private:
+protected:
+    std::vector<std::string> pegs;
     std::string player;
     std::string winner;
-    std::vector<std::string> pegs{9, " "};
 
-    void set_next_player();
+    virtual bool check_column_win();
+    virtual bool check_row_win();
+    virtual bool check_diagonal_win();
+
+private:
     bool check_board_full();
-    void clear_board();
-
-    bool check_row_win();
-    bool check_column_win();
-    bool check_diagonal_win();
-
-    void set_winner();
+    void set_next_player();
 };
 
 #endif
+
