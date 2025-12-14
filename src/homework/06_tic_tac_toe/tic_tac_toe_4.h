f2d0b214ab0d43673ce2d238f1b8a9fcc837e631
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-12-13 21:10:56 -0600

    Add header file for TicTacToe4 class

--- src/homework/06_tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,17 @@
-//h
+#ifndef TIC_TAC_TOE_4_H
+#define TIC_TAC_TOE_4_H
+
+#include "tic_tac_toe.h"
+
+class TicTacToe4 : public TicTacToe
+{
+public:
+    TicTacToe4();
+
+private:
+    bool check_row_win() override;
+    bool check_column_win() override;
+    bool check_diagonal_win() override;
+};
+
+#endif
