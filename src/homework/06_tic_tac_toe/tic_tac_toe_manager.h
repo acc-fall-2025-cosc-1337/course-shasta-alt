3c8bf1e8042ac00473392caf86422b20db8c66f2
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-12-13 21:11:38 -0600

    Update TicTacToeManager to use unique_ptr
    Refactor TicTacToeManager to use smart pointers for game management.

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,23 +1,18 @@
 #ifndef TIC_TAC_TOE_MANAGER_H
 #define TIC_TAC_TOE_MANAGER_H
 
-#include "tic_tac_toe.h"
+#include <memory>
 #include <vector>
-#include <string>
+#include "tic_tac_toe.h"
 
 class TicTacToeManager
 {
 public:
-    void save_game(TicTacToe game);
-    void get_winner_total(int& x, int& o, int& t);
+    void save_game(std::unique_ptr<TicTacToe>& game);
+    void display_games() const;
 
 private:
-    std::vector<TicTacToe> games;
-    int x_win{0};
-    int o_win{0};
-    int ties{0};
-
-    void update_winner_count(std::string winner);
+    std::vector<std::unique_ptr<TicTacToe>> games;
 };
 
 #endif
